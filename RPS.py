daa06abc72b6867bf0a04af41c9e550dd03723ef
newbizme <31482401+newbizme@users.noreply.github.com>
2025-10-09 03:57:56 +0700

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,10 +1,23 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
-
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
-
-    return guess
+    # Сохраняем историю ходов противника
+    if prev_play:
+        opponent_history.append(prev_play)
+    # Для первых ходов делаем последовательные попытки, чтобы собрать паттерн
+    if len(opponent_history) < 3:
+        return "R"
+    # Анализ частоты ходов противника
+    last = opponent_history[-1]
+    freq = {"R": 0, "P": 0, "S": 0}
+    for move in opponent_history:
+        freq[move] += 1
+    # Против частовстречающихся ходов выбираем победный
+    if freq["R"] > freq["P"] and freq["R"] > freq["S"]:
+        return "P"  # Побеждает камень
+    if freq["P"] > freq["R"] and freq["P"] > freq["S"]:
+        return "S"  # Побеждает бумагу
+    if freq["S"] > freq["R"] and freq["S"] > freq["P"]:
+        return "R"  # Побеждает ножницы
+    # Если все одинаково — случайный ход
+    return random.choice(["R", "P", "S"])
